85edcda9c67ed19eb176f22ad0737f35d43aa1ef
Alan Kydd <kydd.alan.g@gmail.com>
2026-01-14 21:54:06 -0700

    Better shutdown of net/http server

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os/signal"
 	"server/node"
+	"syscall"
+	"time"
 )
 
 var (
@@ -14,27 +18,50 @@ var (
 	bindAddr string
 )
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from %s\n", r.URL.Path)
+}
+
 func main() {
 	flag.IntVar(&port, "port", 8080, "port for this service")
 	flag.StringVar(&nodeID, "id", "", "unique node identifier")
 	flag.StringVar(&bindAddr, "baddr", "", "bind address for the node")
 	flag.Parse()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from %s", r.URL.Path)
-	})
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", testHandler)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: mux,
+	}
 
 	_, err := node.New(&node.Config{
 		ID:       nodeID,
 		BindAddr: bindAddr,
 	})
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Server started on port %d\n", port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
-		log.Fatal(err)
+	go func() {
+		fmt.Printf("Server started on port %d\n", port)
+		if err = server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err = server.Shutdown(shutdownCtx); err != nil {
+		fmt.Println("Forced shutdown ...")
+	} else {
+		fmt.Println("Graceful shutdown ...")
 	}
 }
